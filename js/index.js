93e6b17e928a9cfd0f17e27b0be6ed00a92af5b4
Harold Torres Marino <haroldtm55@gmail.com>
2021-06-11 16:46:02 +1000

    Comments added for explanation

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
+  // On page load, render the book names from the JSON throguh renderBooks() from a GET request
   fetch('http://localhost:3000/books')
     .then(resp => resp.json())
     .then(books => {
       renderBooks(books)
     })
-});
+})
 
 function renderBooks(books) {
+  /* Takes the JSON file and  appends each book name to the DOM,
+  Additionally, it renders the Books Info through renderBooksInfo() by using an addEventListener*/
   books.forEach(book => document.getElementById('list').appendChild(document.createElement('li')).textContent = book.title)
   const bookNames = document.querySelectorAll('ul li')
   bookNames.forEach(bookName => {
@@ -21,6 +24,9 @@ function renderBooks(books) {
 }
 
 function renderBooksInfo(books,bookName) {
+  /* Two args: the books JSON file and the bookName from the DOM.
+  It takes the title properties of the JSON file and compare it to the Book names of the DOM. If true,
+  it will manipulate the DOM accordingly with the help of appendUsersLikes() and likeUpdater() */
   const showPanel = document.getElementById('show-panel')
   showPanel.innerHTML = ''
   books.forEach(book => {
@@ -56,19 +62,21 @@ function renderBooksInfo(books,bookName) {
 }
 
 function appendUsersLikes(book) {
+  /* Appends the username property of the users property of the JSON file  */
   book.users.forEach(user => {
     document.querySelector('#show-panel ul').appendChild(document.createElement('li')).textContent = user.username
   })
 }
 
 function likeUpdater(book) {
+  /* Sends a PATCH request to update the JSON file*/
   const configObj = {
     method: 'PATCH',
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json'
     },
-    body: JSON.stringify(book)
+    body: JSON.stringify(book.users) //Seems it works if you use the entire JSON file. (Less efficient????)
   }
   fetch(`http://localhost:3000/books/${book.id}`, configObj)
 }
